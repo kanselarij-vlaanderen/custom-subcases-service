776be4a1c4af4556ab1d11a7dec8a7a498b1004f
ValenberghsSven <sven.valenberghs1@gmail.com>
2020-06-15 16:27:19 +0200

    KAS-1425 search postponed based on retracted property and through activity

--- repository/index.js
+++ repository/index.js
@@ -1,27 +1,20 @@
 import mu from 'mu';
-const targetGraph = "http://mu.semte.ch/graphs/organizations/kanselarij";
 
 const getPostponedSubcases = async () => {
 
     const query = `
-       PREFIX vo-org: <https://data.vlaanderen.be/ns/organisatie#>
       PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-      PREFIX vo-gen: <https://data.vlaanderen.be/ns/generiek#> 
-      PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
       PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
-      PREFIX agenda: <http://data.lblod.info/id/agendas/>
-      PREFIX mandaat: <http://data.vlaanderen.be/ns/mandaat#>
-      PREFIX dct: <http://purl.org/dc/terms/>
+      PREFIX dbpedia: <http://dbpedia.org/ontology/>  
       
-      SELECT ?id  
-        WHERE { 
-          GRAPH <${targetGraph}>
-          {
-            ?subcase besluitvorming:isGeagendeerdVia ?agendapunt ;
-             mu:uuid ?id .
-            ?agendapunt ext:heeftVerdaagd ?verdaagd .
-           }
+      SELECT ?id WHERE { 
+          ?subcase a dbpedia:UnitOfWork ;
+            mu:uuid ?id ;
+            besluitvorming:vindtPlaatsTijdens ?activity .
+          ?activity besluitvorming:genereertAgendapunt ?agendapunt .
+            
+          ?agendapunt besluitvorming:ingetrokken ?retracted . 
+          FILTER(?retracted ="true"^^<http://mu.semte.ch/vocabularies/typed-literals/boolean>)  
       } GROUP BY ?subcase`;
 
     let data = await mu.query(query);
